9ef5753814dbcf4eec124a3f73cf4c58304f757e
n.naumov <n.naumov@timepad.ru>
2022-06-23 02:50:50 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [title, setTitle] = useState<string>('')
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/items').then(res => setTitle(res.data.title))
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Helloalekum, {title}</h1>
+    </div>
+  );
+}
+
+export default App;
